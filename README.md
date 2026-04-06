2601c7c70413d53e29df6a0a061cd4fe901aa925
daanbp96 <DaanBarsukoff@gmail.com>
2026-04-06 22:41:37 +0700

    testing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # readtard-app
+
+
---
+++ Readtard/ReadtardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadtardApp.swift
+//  Readtard
+//
+//  Created by Daan Barsukoff Poniatowsky on 06/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ReadtardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
